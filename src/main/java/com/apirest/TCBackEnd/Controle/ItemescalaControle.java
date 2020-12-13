74e4c1a623a93d34eefc47bd30370d80f42b82fb
Erik Endler <eng.erik.endler@gmail.com>
2020-12-13 13:58:01 -0300

    ...

--- src/main/java/com/apirest/TCBackEnd/Controle/ItemescalaControle.java
+++ src/main/java/com/apirest/TCBackEnd/Controle/ItemescalaControle.java
@@ -122,9 +122,10 @@ public class ItemescalaControle extends GenericControl<ItemEscala, ItemEscalaDTO
 	private void verificaSobreposiçao(ItemEscalaDTO dto) {
 		int qtd = repositorio.escalaByHrInicialAndHrfinal(datahora.stringEmHora(dto.getHrInicial()),
 				datahora.stringEmHora(dto.getHrFinal()), dto.getEscala());
-		System.out.println("QTD da busca de comflito : "+qtd);
+		System.out.println("QTD da busca de comflito : " + qtd);
 		if (qtd > 0) {
 			System.out.println("----Disparada exceção de conflito de escalas");
+			System.out.println(new ResourceNotFoundException("Escala com horario conflitante"));
 			throw new ResourceNotFoundException("Escala com horario conflitante");
 		}
 	}
