04f27a85005c628f2175aa20df7787c70a7544e4
ErikEndler <45501252+ErikEndler@users.noreply.github.com>
2020-12-30 13:44:22 -0300

    ResourceNotFoundException

--- src/main/java/com/apirest/TCBackEnd/Util/Error/ResourceExceptionHandler.java
+++ src/main/java/com/apirest/TCBackEnd/Util/Error/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -24,17 +25,17 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
-//	@ExceptionHandler(ResourceNotFoundException.class)
-//	public ResponseEntity<StandardError> resourceNotFound(EntityNotFoundException e, HttpServletRequest request) {
-//		StandardError err = new StandardError();
-//		err.setTimestamp(Instant.now());
-//		err.setStatus(HttpStatus.NOT_FOUND.value());
-//		err.setError("Resource not found");
-//		err.setMessage(e.getMessage());
-//		err.setPath(request.getRequestURI());
-//
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-//	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(EntityNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
 
 }
